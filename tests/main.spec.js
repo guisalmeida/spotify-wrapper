bee32533a87a605859fda51f47fab42e277c69b2
guisalmeida <guilherme.dasilva@linx3.com>
2020-07-30 21:13:46 -0300

    make fetch tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,37 +1,58 @@
-describe('Main', function() {  //separa grandes blocos
-  describe('Method a', function() {
-    context.skip('Case 1', function() {
-      it('should happen bla', function() {
-        //espera que aconteça algo
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai'; // integração sinon com chai
+import sinonStubPromise from 'sinon-stub-promise'; // para trabalhar com promise
+chai.use(sinonChai); //chai que asserts do sinonchai
+sinonStubPromise(sinon); // recebe todos objetos do sinon
 
-        //entra dados
+global.fetch = require('node-fetch');
 
-        //espera retornar
-        throw new Error("error");
-      })
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+
+describe('Spotify wrapper', () => {
+
+  describe('smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.be.exist;
     });
 
-    context.skip('Case 2', function() {
-      it('should happen blabla', function() {
-        //espera que aconteça algo
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.be.exist;
+    });
 
-        //entra dados
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.be.exist;
+    });
 
-        //espera retornar
-        throw new Error("error");
-      })
-    })
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.be.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.be.exist;
+    });
   });
 
-  describe('Method b', function() {
-    context('Case1', function() {
-      it('should happen mimimi', function() {
-        //espera que aconteça algo
+  describe('Generic search', () => {
+    it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search();
 
-        //entra dados
+      expect(fetchedStub).to.have.been.calledOnce;
 
-        //espera retornar
-      })
-    })
-  });
+      fetchedStub.restore();
+    });
+
+    it('should receive the correct url to fetch', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search('Incubus', 'artist');
+
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+      const albuns = search('Incubus', 'album');
+
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album')
+    });
+
+  })
 })
